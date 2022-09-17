3f5fbd8760efb08fcaf1d317bf82bdab293c3116
Muhammad Fauzan Aristya Putra <muhammadfauzanaristyaputra@gmail.com>
2022-09-17 11:58:02 +0300

    stop reset

--- aws.js
+++ aws.js
@@ -1,13 +1,3 @@
 const aws = require('aws-sdk');
 const s3 = new aws.S3();
-s3.putObject({
-	Key: 'data.json',
-	Bucket: process.env.AWS_BUCKET,
-	Body: JSON.stringify({users:{}}),
-  }).promise();
-  s3.putObject({
-	Key: 'keys.json',
-	Bucket: process.env.AWS_BUCKET,
-	Body: JSON.stringify({}),
-  }).promise();
 module.exports = {aws, s3};
