e042c50aa1b88bca95a789861e758490d4ccb7cc
Muhammad Fauzan Aristya Putra <muhammadfauzanaristyaputra@gmail.com>
2022-09-17 14:04:45 +0300

    asd

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 require('dotenv').config();
 const bodyParser = require("body-parser")
-const cors = require('cors')
 const app = express()
 const port = 5000;
 const { validateEmail } = require('./validation')
