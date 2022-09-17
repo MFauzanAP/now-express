872639e0da65f1d331d800bce8ae4be8d2125d1d
Muhammad Fauzan Aristya Putra <muhammadfauzanaristyaputra@gmail.com>
2022-09-17 13:29:53 +0300

    fds

--- index.js
+++ index.js
@@ -20,7 +20,8 @@ app.options("/register", function(req, res, next){
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-  res.status(200);
+  res.sendStatus(200);
+  next();
 });
 app.post('/register', async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -54,7 +55,8 @@ app.options("/getUserData", function(req, res, next){
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-  res.status(200);
+  res.sendStatus(200);
+  next();
 });
 app.post('/getUserData', async function (req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
