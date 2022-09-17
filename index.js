3c9d5b8399e3a240ef4346a35766423868b71f64
Muhammad Fauzan Aristya Putra <muhammadfauzanaristyaputra@gmail.com>
2022-09-17 13:17:47 +0300

    oiu

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
 
-  if (req.method === 'OPTIONS') res.send(200);
+  if (req.method === 'OPTIONS') res.status(200);
   else next();
 })
 // users.rankUser('1020365193768874055');
