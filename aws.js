25489fe540d8a74f9821b029a70a61a153c3300e
Muhammad Fauzan Aristya Putra <muhammadfauzanaristyaputra@gmail.com>
2022-09-17 12:50:22 +0300

    fds

--- aws.js
+++ aws.js
@@ -1,3 +1,13 @@
 const aws = require('aws-sdk');
 const s3 = new aws.S3();
+s3.putObject({
+	Key: 'data.json',
+	Bucket: process.env.AWS_BUCKET,
+	Body: JSON.stringify({users:{}}),
+  }).promise();
+  s3.putObject({
+	Key: 'keys.json',
+	Bucket: process.env.AWS_BUCKET,
+	Body: JSON.stringify({}),
+  }).promise();
 module.exports = {aws, s3};
